4a7448f6f482e1f7fbe9613d5191fb722369df54
Daniel <110098882+deshchk@users.noreply.github.com>
2023-09-05 22:24:55 +0200

    hover when supported

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,7 @@ export const content = ["./deployment/*.{html,js}", "./deployment/js/*.js"]
 export const theme = {
   extend: {},
 }
+export const future = {
+  hoverOnlyWhenSupported: true,
+}
 export const plugins = []
